c8be7ab5619c4c87cd3a219f2f0ec3b718ba571b
Elyance <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-06-24 12:34:00 +0300

    connexion base et structure Flight, creation branche dev

---
+++ app/config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use app\controllers\ApiExampleController;
+use app\controllers\WelcomeController;
+use flight\Engine;
+use flight\net\Router;
+//use Flight;
+
+/** 
+ * @var Router $router 
+ * @var Engine $app
+ */
+/*$router->get('/', function() use ($app) {
+	$Welcome_Controller = new WelcomeController($app);
+	$app->render('welcome', [ 'message' => 'It works!!' ]);
+});*/
+
+$Welcome_Controller = new WelcomeController();
+$router->get('/', [ $Welcome_Controller, 'home' ]); 
+$router->get('/truie', [ $Welcome_Controller, 'truie' ]);
---
+++ app/controllers/WelcomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Truie;
+use Flight;
+
+class WelcomeController {
+    public function home() {
+        Flight::render('welcome', [
+            'message' => 'It works!!',
+        ]);
+    }
+
+    public function truie() {
+        $truies = Truie::getAll();
+        Flight::render('truie', [
+            'truies' => $truies,
+        ]);
+    }
+}
