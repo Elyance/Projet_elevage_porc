cd43f328a518666ec684ef89fdeb8115a614e030
SafidyPatrickRas <safidypatrickrasolofoniaina@gmail.com>
2025-06-24 21:31:04 +0300

    juste rien du tout

--- app/views/enclos/index.php
+++ app/views/enclos/index.php
@@ -1,5 +1,5 @@
 <div class="col-lg-10">
-    <h2>List des enclos</h2>
+    <h2>Enclos</h2>
     <a href="/enclos/add"> <button type="button" class="btn mb-1 btn-outline-secondary">ajouter un enclos</button></a>
     <a href="/enclos/"> <button type="button" class="btn mb-1 btn-outline-secondary">List des enclos</button></a>
     <a href=""> <button type="button" class="btn mb-1 btn-outline-secondary">Deplacer Porc</button></a>
