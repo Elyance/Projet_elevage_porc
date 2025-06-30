708ea54e7dc9a65051bf514964506bd125359a74
Rakoto <tenaizyrakoto@gmail.com>
2025-06-30 16:52:07 +0300

    First commit

--- app/config/config.php
+++ app/config/config.php
@@ -73,7 +73,7 @@ return [
 		 'host' => 'localhost',
 		 'dbname' => 'gestion_porc',
 		 'user' => 'postgres',
-		 'password' => ' '
+		 'password' => 'rakoto'
 
 		// uncomment the following line for sqlite
 		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite'
