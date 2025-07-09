9a6ba2847b31d3432c308ced3b874243395dd508
Ray <raymatatagios@gmail.com>
2025-07-09 03:14:51 +0300

    Placeholder home

--- app/views/home.php
+++ app/views/home.php
@@ -1,6 +1,2 @@
-<?php
-require_once __DIR__ . '/partials/header.php'; ?>
-
-<h1 class="text-2xl mb-4">Bienvenue dans le système de gestion porcin</h1>
-<p>Utilisez le menu pour naviguer.</p>
-<?php require_once __DIR__ . '/partials/footer.php'; ?>
+<?php ?>
+Welcome
