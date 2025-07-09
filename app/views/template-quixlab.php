3c4b803414381511ee36dee6d510406eb6420b3e
Ray <raymatatagios@gmail.com>
2025-07-09 03:37:11 +0300

    Added profile image and name display

--- app/views/template-quixlab.php
+++ app/views/template-quixlab.php
@@ -1,10 +1,4 @@
 <?php 
-    session_start();
-
-    if (!isset($_SESSION['user_id']) || !isset($_SESSION['user'])) {
-        Flight::redirect('/login');
-        exit();
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,6 +47,35 @@
                     <div class="hamburger">
                         <span class="toggle-icon"><i class="icon-menu"></i></span>
                     </div>
+                </div>
+                <div class="header-right">
+                    <ul class="clearfix">
+                        <li class="icons dropdown">
+                            <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
+                                <span class="activity active"></span>
+                                <?php 
+                                    $imagePath = '';
+                                    if (isset($_SESSION['user_role_id']) && $_SESSION['user_role_id'] == 1) {
+                                        $imagePath = STATIC_URL.'/assets/images/Henloalt.png';
+                                    } else {
+                                        $imagePath = STATIC_URL.'/assets/images/Henlo.png';
+                                    }
+                                ?>
+                                <img src="<?= $imagePath ?>" height="40" width="40" alt="">
+                            </div>
+                            <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
+                                <div class="dropdown-content-body">
+                                    <ul>
+                                        <li>
+                                            <i class="icon-user"></i> <span><?= $_SESSION['user'] ?></span>
+                                        </li>
+                                        <hr class="my-2">
+                                        <li><a href="<?= BASE_URL ?>/logout"><i class="icon-key"></i> <span>Logout</span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
