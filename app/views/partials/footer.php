da502ab8c97c0e0d914ecdda5723e2303878f041
Ray <raymatatagios@gmail.com>
2025-07-09 07:21:47 +0300

    Deleted some files

--- app/views/partials/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer mt-auto p-4 bg-gradient-to-r from-primary to-dark text-center text-sm text-secondary animate-fade-in">
-    &copy; <?= date('Y') ?> GestionPorc. Tous droits réservés.
-</footer>
-</main>
-<script src="/app/views/js/script.js"></script>
-</body>
-</html>
